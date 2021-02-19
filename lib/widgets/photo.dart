efde3a3134a2cab426411a09f9ff25fc5224cfc8
Saygin_DI <Saygin_DI@cd.local>
2021-02-19 08:17:47 +0300

    Цветной плейсхолдер из ответа

--- lib/widgets/photo.dart
+++ lib/widgets/photo.dart
@@ -9,8 +9,6 @@ class Photo extends StatelessWidget {
     this.paddingHorizontal = 10,
     this.paddingVertical = 5,
     this.borderRadius = 17,
-    this.width,
-    this.height,
     this.placeholderColor = Colors.black26,
   }) : super(key: key);
 
@@ -18,33 +16,23 @@ class Photo extends StatelessWidget {
   final double paddingHorizontal;
   final double paddingVertical;
   final double borderRadius;
-  double width;
-  double height;
   final Color placeholderColor;
 
   @override
   Widget build(BuildContext context) {
-    if (width != null && height != null) {
-      width = MediaQuery.of(context).size.width;
-      height = MediaQuery.of(context).size.width / width * height;
-      width = MediaQuery.of(context).size.width;
-    }
-
     return Padding(
         padding: EdgeInsets.symmetric(
             horizontal: paddingHorizontal, vertical: paddingVertical),
         child: ClipRRect(
           borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
           child: CachedNetworkImage(
-            height: height,
-            width: width,
             imageUrl: photoLink,
             fit: BoxFit.fill,
             placeholder: (context, url) => Center(
               child: Container(
-                child: Container(
-                  color: this.placeholderColor,
-                ),
+                height: MediaQuery.of(context).size.width,
+                width: MediaQuery.of(context).size.width,
+                color: this.placeholderColor,
               ),
             ),
             errorWidget: (context, url, error) => Icon(Icons.error),
