af75ccc6b3d38546d284dd59ab6df0e4971b4750
Saygin_DI <Saygin_DI@cd.local>
2021-02-19 07:29:57 +0300

    Цветной плейсхолдер

--- lib/widgets/photo.dart
+++ lib/widgets/photo.dart
@@ -4,18 +4,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Photo extends StatelessWidget {
-  Photo(
-      {Key key,
-      this.photoLink,
-      this.paddingHorizontal = 10,
-      this.paddingVertical = 5,
-      this.borderRadius = 17})
-      : super(key: key);
+  Photo({
+    Key key,
+    this.photoLink,
+    this.paddingHorizontal = 10,
+    this.paddingVertical = 5,
+    this.borderRadius = 17,
+    this.placeholderColor = Colors.black26,
+  }) : super(key: key);
 
   final String photoLink;
   final double paddingHorizontal;
   final double paddingVertical;
   final double borderRadius;
+  final Color placeholderColor;
 
   @override
   Widget build(BuildContext context) {
@@ -29,8 +31,19 @@ class Photo extends StatelessWidget {
             child: CachedNetworkImage(
               imageUrl: photoLink,
               fit: BoxFit.fill,
-              placeholder: (context, url) =>
-                  Center(child: CircularProgressIndicator()),
+              placeholder: (context, url) => Center(
+                child: Container(
+                  height: MediaQuery.of(context).size.width,
+                  width: MediaQuery.of(context).size.width,
+                  child: ClipRRect(
+                    borderRadius:
+                        BorderRadius.all(Radius.circular(borderRadius)),
+                    child: Container(
+                      color: this.placeholderColor,
+                    ),
+                  ),
+                ),
+              ),
               errorWidget: (context, url, error) => Icon(Icons.error),
             ),
           )),
