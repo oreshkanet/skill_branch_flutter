6b9fba8bb186067442809d270e570fe8616ab6d5
Saygin_DI <Saygin_DI@cd.local>
2021-04-08 08:03:53 +0300

    Progress indicator customization

--- lib/widgets/progress_indicator.dart
+++ lib/widgets/progress_indicator.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ProgressIndicatorWidget extends StatelessWidget {
+class ProgressIndicatorWidget extends StatefulWidget {
+  const ProgressIndicatorWidget({Key key}) : super(key: key);
+
+  @override
+  _ProgressIndicatorWidgetState createState() =>
+      _ProgressIndicatorWidgetState();
+}
+
+class _ProgressIndicatorWidgetState extends State<ProgressIndicatorWidget>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    )..addListener(() {
+        setState(() {});
+      });
+    _controller.repeat(reverse: true);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CircularProgressIndicator(),
+    return CircularProgressIndicator(
+      value: _controller.value,
     );
   }
 }
